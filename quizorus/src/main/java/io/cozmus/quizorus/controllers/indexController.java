739dccfae8f4777faa58d3119e493ea75f99547e
cozmus <cozmus@gmail.com>
2019-03-03 20:53:21 +0300

    authentication pages and controllers created

--- quizorus/src/main/java/io/cozmus/quizorus/controllers/indexController.java
+++ quizorus/src/main/java/io/cozmus/quizorus/controllers/indexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class indexcontroller {
+public class indexController {
 
     @GetMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage(){
