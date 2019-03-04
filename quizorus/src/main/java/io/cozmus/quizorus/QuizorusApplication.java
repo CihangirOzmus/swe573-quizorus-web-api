ff819751c7a413f63103350ba0710350b46d4af4
cozmus <cozmus@gmail.com>
2019-03-04 19:16:01 +0300

    starting chrome automatically implemented for windows in comments

--- quizorus/src/main/java/io/cozmus/quizorus/QuizorusApplication.java
+++ quizorus/src/main/java/io/cozmus/quizorus/QuizorusApplication.java
@@ -8,6 +8,25 @@ public class QuizorusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizorusApplication.class, args);
+		//openIndexPage("http://localhost:8080/");
 	}
+
+//	private static void openIndexPage(String url) {
+//		if(Desktop.isDesktopSupported()){
+//			Desktop desktop = Desktop.getDesktop();
+//			try {
+//				desktop.browse(new URI(url));
+//			} catch (IOException | URISyntaxException e) {
+//				e.printStackTrace();
+//			}
+//		}else{
+//			Runtime runtime = Runtime.getRuntime();
+//			try {
+//				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
 
 }
