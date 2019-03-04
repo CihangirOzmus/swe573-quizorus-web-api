a1c107cd61d8c7f0126c2baa76839e91ed96d8be
cihangiro <cozmus@gmail.com>
2019-03-04 10:41:23 +0300

    how to open browser automatically

--- quizorus/src/main/java/io/cozmus/quizorus/QuizorusApplication.java
+++ quizorus/src/main/java/io/cozmus/quizorus/QuizorusApplication.java
@@ -3,11 +3,35 @@ package io.cozmus.quizorus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class QuizorusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizorusApplication.class, args);
+		openIndexPage("http://localhost:8080/");
+	}
+
+	private static void openIndexPage(String url) {
+		if(Desktop.isDesktopSupported()){
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URI(url));
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}else{
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
