314b44eba468df3e59a57068b61c885b5fa2c402
Cihangir Ozmus <cozmus@gmail.com>
2019-05-03 20:37:26 +0300

    Spring Boot updated to 2.1.4

--- backend/src/main/java/com/quizorus/backend/model/Question.java
+++ backend/src/main/java/com/quizorus/backend/model/Question.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.quizorus.backend.model.audit.UserDateAudit;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.List;
